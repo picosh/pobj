1dc70e819bbf916b8968c1507a062e0e5a493539
Eric Bower <me@erock.io>
2024-02-18 10:03:08 -0500

    chore: cleanup

--- storage/storage.go
+++ storage/storage.go
@@ -15,11 +15,11 @@ type Bucket struct {
 
 type ObjectStorage interface {
 	GetBucket(name string) (Bucket, error)
+	GetBucketQuota(bucket Bucket) (uint64, error)
 	UpsertBucket(name string) (Bucket, error)
 	ListBuckets() ([]string, error)
-
 	DeleteBucket(bucket Bucket) error
-	GetBucketQuota(bucket Bucket) (uint64, error)
+
 	GetObject(bucket Bucket, fpath string) (utils.ReaderAtCloser, int64, time.Time, error)
 	PutObject(bucket Bucket, fpath string, contents utils.ReaderAtCloser, entry *utils.FileEntry) (string, error)
 	DeleteObject(bucket Bucket, fpath string) error
